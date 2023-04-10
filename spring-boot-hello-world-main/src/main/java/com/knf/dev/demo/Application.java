d903cf816f13f63b4bfd8bf81160d7074635ccd1
Nittala Naga Sreeram <nnsreeram@digisprint.com>
2023-04-10 16:37:35 +0530

    updated swagger dependency

--- spring-boot-hello-world-main/src/main/java/com/knf/dev/demo/Application.java
+++ spring-boot-hello-world-main/src/main/java/com/knf/dev/demo/Application.java
@@ -3,7 +3,15 @@ package com.knf.dev.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Demo Api",
+	version = "v1",
+	description = "These are APIs for Demo"),
+	servers = @Server(url = "/"))
 public class Application {
 
 	public static void main(String[] args) {
